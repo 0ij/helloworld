eb1569c9d2bbcc5d18f894f24883ee5d21f2ac04
0ij <3557444670@qq.com>
2026-03-22 18:18:00 +0800

    fix: site github url

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 export const SITE_TITLE = 'My Learning Note';
 export const SITE_DESCRIPTION = 'KEEPING LEARNING'
 export const SITE_AUTHOR = 'Hekooo';
-export const SITE_GITHUB_URL = 'https://github.com/<your-name>/<your-repo>';
-export const SITE_EMAIL = 'hello@example.com';
+export const SITE_GITHUB_URL = 'https://github.com/0ij/helloworld';
+export const SITE_EMAIL = '3557444670@qq.com';
